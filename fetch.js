b3f44137ab1f8b3243aa1a358b0ac3c12c787c2e
nmparfait <mbamu.developer@gmail.com>
2021-04-26 13:07:37 -0400

    using async await to get the same result of random

--- fetch.js
+++ fetch.js
@@ -3,14 +3,11 @@ const img = document.querySelector('.container img');
 const content = document.querySelector('.content');
 const URL = 'https://api.chucknorris.io/jokes/random';
 
-btn.addEventListener('click', async () => {
-  try {
-    const data = await fetch(URL);
-    const response = await data.json();
-    displayData(response);
-  } catch (error) {
-    console.log(error);
-  }
+btn.addEventListener('click', () => {
+  fetch(URL)
+    .then((data) => data.json())
+    .then((response) => displayData(response))
+    .catch((err) => console.log(err));
 });
 
 function getData(url) {
